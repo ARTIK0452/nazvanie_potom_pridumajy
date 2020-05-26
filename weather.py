c605bab2690dc3f5af72a93e9780eb9cdc7dfaca
ARTIK0452 <43519433+ARTIK0452@users.noreply.github.com>
2020-05-26 16:01:28 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+#v.1.0
 #5d9d9f7178756dbc78d68958da985841
 listKey = ['weather']
 
@@ -14,3 +15,33 @@ try:
 except Exception as e:
 	print("Exception (find):", e)
 	pass
+
+#-----------------------------------------------------------------------------------------------------
+#v.1.1
+
+#5d9d9f7178756dbc78d68958da985841
+listKey = ['weather']
+
+import requests
+from geopy.geocoders import Nominatim
+
+appid = "5d9d9f7178756dbc78d68958da985841"
+
+def weather_data(lantitude, lontitude):
+	try:
+		res = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + str(lantitude) + '&lon=' + str(lontitude) + '&exclude=hourly,daily&units=metric&appid=' + appid)
+		#print(res.text)
+		data = res.json()
+		for k, v in data.items():
+			print(v['humidity'])
+	except Exception as e:
+		print("Exception (find):", e)
+		
+def geo_location():
+	locator = Nominatim (user_agent = 'myGeocoder')
+	location = locator.geocode ('Moscow')
+	return location.latitude, location.longitude
+	
+lat, lon = geo_location()
+weather_data(lat, lon)
+
